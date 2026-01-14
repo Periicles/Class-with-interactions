244fd05d295b9fe2ea0de95e5192d1bdaa1e2b1b
Théo Gillet <gillettheo1@gmail.com>
2026-01-14 14:46:16 +0100

    :white_check_mark: GREEN - Add test task with missing field

--- src/Scheduler.ts
+++ src/Scheduler.ts
@@ -3,7 +3,13 @@ import { Task } from "./types/task.types";
 export class Scheduler {
   constructor(
     private tasks: Task[]
-  ) { }
+  ) {
+    for (const task of tasks) {
+      if (!task.name || !task.periodicity || !task.callback) {
+        throw new Error("Each task must have name, periodicity and callback properties.");
+      }
+    }
+  }
 
   getTasks(): Task[] {
     return this.tasks;
