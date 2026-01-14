54ff30c257fd81381abad598d582bfbfa86e190f
Théo Gillet <gillettheo1@gmail.com>
2026-01-14 12:29:47 +0100

    :white_check_mark: GREEN - Add test task on init

--- src/tests/Scheduler.test.ts
+++ src/tests/Scheduler.test.ts
@@ -1,16 +1,16 @@
 import { describe, it, expect } from "@jest/globals";
 import { Scheduler } from "../Scheduler";
+import { Task } from "../types/task.types";
 describe("Scheduler", () => {
   it("should create a Scheduler instance", () => {
-    const scheduler = new Scheduler();
+    const scheduler = new Scheduler([]);
     expect(scheduler).toBeInstanceOf(Scheduler);
   })
 
   it("should set and get tasks in init", () => {
-    const tasks = []
-    const scheduler = new Scheduler();
+    const tasks: Task[] = []
+    const scheduler = new Scheduler(tasks);
     const schedulerTasks = scheduler.getTasks();
     expect(schedulerTasks).toEqual(tasks);
-  })
-
+  });
 })
