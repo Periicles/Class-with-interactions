ab6d1c090e3e92169ee288a42455f778dca55757
Théo Gillet <gillettheo1@gmail.com>
2026-01-14 12:12:24 +0100

    :white_check_mark: GREEN - Add test scheduler instance

--- src/tests/Scheduler.test.ts
+++ src/tests/Scheduler.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from "@jest/globals";
+import { Scheduler } from "../Scheduler";
 describe("Scheduler", () => {
   it("should create a Scheduler instance", () => {
     const scheduler = new Scheduler();
